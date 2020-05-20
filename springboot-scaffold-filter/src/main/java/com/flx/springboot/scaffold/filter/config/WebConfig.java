4d7b494419c110dad5b0401ca289372a26b08d66
fenglixiong <fenglixiong@flashhold.com>
2020-05-20 20:12:07 +0800

    springboot拦截器使用

---
+++ springboot-scaffold-filter/src/main/java/com/flx/springboot/scaffold/filter/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.flx.springboot.scaffold.filter.config;
+
+import com.flx.springboot.scaffold.filter.inteceptor.InterceptorOne;
+import com.flx.springboot.scaffold.filter.inteceptor.InterceptorTwo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: Fenglixiong
+ * @Date: 2020/5/20 19:51
+ * @Description:
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private InterceptorOne interceptorOne;
+    @Autowired
+    private InterceptorTwo interceptorTwo;
+
+    /**
+     * 添加拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptorOne).addPathPatterns("/**")
+                .excludePathPatterns("/login").excludePathPatterns("/img/**","/css/**","/js/**");
+        registry.addInterceptor(interceptorTwo).addPathPatterns("/**")
+                .excludePathPatterns("/login").excludePathPatterns("/img/**","/css/**","/js/**");
+    }
+}
---
+++ springboot-scaffold-filter/src/main/java/com/flx/springboot/scaffold/filter/inteceptor/InterceptorOne.java
@@ -0,0 +1,38 @@
+package com.flx.springboot.scaffold.filter.inteceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: Fenglixiong
+ * @Date: 2020/5/20 19:46
+ * @Description:
+ */
+@Slf4j
+@Component
+public class InterceptorOne implements HandlerInterceptor {
+
+    //进入controller层之前拦截请求
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("InterceptorOne preHandle ...");
+        return true;
+    }
+
+    //处理请求完成后视图渲染之前的处理操作
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("InterceptorOne postHandle ...");
+    }
+
+    //视图渲染之后的操作
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("InterceptorOne afterCompletion ...");
+    }
+}
