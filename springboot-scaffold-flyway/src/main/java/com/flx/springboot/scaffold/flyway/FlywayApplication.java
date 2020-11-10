ff02a26839d8efa978dc9b165952542d93659c94
fenglixiong <fenglixiong@flashhold.com>
2020-11-10 14:19:52 +0800

    flyway顺序问题

--- springboot-scaffold-flyway/src/main/java/com/flx/springboot/scaffold/flyway/FlywayApplication.java
+++ springboot-scaffold-flyway/src/main/java/com/flx/springboot/scaffold/flyway/FlywayApplication.java
@@ -6,6 +6,11 @@ import lombok.extern.slf4j.Slf4j;
  * @Author: Fenglixiong
  * @Date: 2020/8/4 17:53
  * @Description:
+ * 解决flyway启动顺序问题
+ * 1.depenson
+ *  @DependsOn(value = "flywayInitializer")
+ * 2.自己写flyway配置
+ * @SpringBootApplication(exclude = {FlywayAutoConfiguration.class})
  */
 @Slf4j
 public class FlywayApplication {
