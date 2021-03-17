380a95e308ff29a823816d3e17498572f5998224
fenglixiong <fenglixiong123@163.com>
2021-03-17 23:52:41 +0800

    优化代码

--- springboot-scaffold-common/src/main/java/com/flx/springboot/scaffold/common/utils/network/IPUtils.java
+++ springboot-scaffold-common/src/main/java/com/flx/springboot/scaffold/common/utils/network/IPUtils.java
@@ -39,7 +39,7 @@ public class IPUtils {
         return ip;
     }
 
-    public static String getPerfertIp(HttpServletRequest request) {
+    public static String getPerfectIp(HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
         if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
